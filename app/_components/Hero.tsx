ebc53168a811f9df8700c0de424377cbac7a70a3
E232173M <kevin.sorin@etu.univ-nantes.fr>
2025-02-06 17:35:41 +0000

    update

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,18 +1,28 @@
+import Image from "next/image";
 import { Section } from "./Section";
 
 export const Hero = () => {
   return (
-    <Section className="flex max-lg:flex-col items-start">
-        <div className="flex-[2]">
-            <h2>Kévin Sorin</h2>
-            <h3>Etudiant en informatique</h3>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laudantium nobis quam? Possimus, consequuntur. Itaque nulla dolore in totam, fugiat nam voluptatem dicta vero voluptas atque illo nesciunt ex? Incidunt?
-            </p>
-        </div>
-        <div className="flex-1">
-
-        </div>
+    <Section className="flex max-md:flex-col items-start">
+      <div className="flex-[2]">
+        <h2 className="font-caption text-5xl">Kévin Sorin</h2>
+        <h3 className="font-caption text-3xl">Etudiant en informatique</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          laudantium nobis quam? Possimus, consequuntur. Itaque nulla dolore in
+          totam, fugiat nam voluptatem dicta vero voluptas atque illo nesciunt
+          ex? Incidunt?
+        </p>
+      </div>
+      <div className="flex-1">
+        <Image
+          src="/images/image_moi_carre.png"
+          className="w-full h-auto rounded-full max-w-xs"
+          alt="image de Kévin"
+          width={256}
+          height={256}
+        />
+      </div>
     </Section>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { Header } from "./_components/Header";
 import { Section } from "./_components/Section";
+import { Hero } from "./_components/Hero";
+import { Spacing } from "./_components/Spacing";
 
 export default function Home() {
   return (
     <main>
-      <Header/>
+      <Header />
+      <Spacing size="md" />
+      <Hero />
     </main>
   );
 }
